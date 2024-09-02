a7629a4831aaa059e8d9bb8d1906a4813966d32c
Max H <139349788+Maxeu444@users.noreply.github.com>
2024-09-02 14:14:26 +0200

    Update ingredient.dart

--- untilted/lib/src/ingredient.dart
+++ untilted/lib/src/ingredient.dart
@@ -4,13 +4,3 @@ class Ingredient {
 
   Ingredient(this.name, this.isVegan);
 }
-
-mixin IngredientMixin {}
-
-class IngredientSubTypeWithMixin with IngredientMixin{
-
-}
-
-class IngredientSubTypeWithExtends extends Ingredient {
-  IngredientSubTypeWithExtends(super.name, super.isVegan); //héritage
-}
